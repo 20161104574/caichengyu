171e6057060965f56c9f7b7b6fc04028974d3e6b
20161104574 <a20161104574@admindeiMac-12.local>
2018-12-26 20:29:35 +0800

    2
    2

--- caichengyu/ViewController.swift
+++ caichengyu/ViewController.swift
@@ -572,6 +572,7 @@ class ViewController: UIViewController {
             r4.text=""
         }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
