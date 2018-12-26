d541d0d17468ad22124f180ccb5fabe49e785d81
20161104574 <a20161104574@admindeiMac-12.local>
2018-12-26 20:24:29 +0800

    caichengyu
    Caichengyu

--- caichengyu/ViewController.swift
+++ caichengyu/ViewController.swift
@@ -9,10 +9,576 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var re = 0
+    @IBOutlet weak var r1: UITextField!
+    
+    @IBOutlet weak var r2: UITextField!
+    
+    @IBOutlet weak var r3: UITextField!
+    
+    @IBOutlet weak var r4: UITextField!
+    
+    @IBOutlet weak var r5: UITextField!
+    
+    @IBOutlet weak var r6: UITextField!
+    
+    @IBAction func b11(_ sender: Any) {
+            if r1.text == ""
+            {
+                r1.text=r1.text!+"马"
+            }
+             if r2.text == ""
+            {
+                r2.text=r2.text!+"马"
+            }
+            else if r3.text == ""
+            {
+                r3.text=r3.text!+"马"
+            }
+            else if r4.text == ""
+            {
+                r4.text=r4.text!+"马"
+            }
+        
+    }
+    
+    
+    @IBAction func b12(_ sender: Any) {
+            if r1.text == ""
+            {
+                r1.text=r1.text!+"千"
+            }
+            else if r2.text == ""
+            {
+                r2.text=r2.text!+"千"
+            }
+            else if r3.text == ""
+            {
+                r3.text=r3.text!+"千"
+            }
+            else if r4.text == ""
+            {
+                r4.text=r4.text!+"千"
+            }
+            re = 1
+    }
+    
+    @IBAction func b13(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"狐"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"狐"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"狐"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"狐"
+        }
+        re = 1
 
+    }
+    
+    @IBAction func b14(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"鬼"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"鬼"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"鬼"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"鬼"
+        }
+        re = 1
+
+    }
+    
+    @IBAction func b15(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"存"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"存"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"存"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"存"
+        }
+        re = 1
+
+    }
+    
+    @IBAction func b21(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"风"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"风"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"风"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"风"
+        }
+        re = 1
+
+    }
+    
+    @IBAction func b22(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"朋"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"朋"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"朋"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"朋"
+        }
+        re = 1
+    }
+    
+    @IBAction func b23(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"哭"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"哭"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"哭"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"哭"
+        }
+        re = 1
+    }
+    
+    @IBAction func b24(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"离"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"离"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"离"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"离"
+        }
+        re = 1
+    }
+    
+    @IBAction func b25(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"为"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"为"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"为"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"为"
+        }
+        re = 1
+    }
+    
+    @IBAction func b31(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"言"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"言"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"言"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"言"
+        }
+        re = 1
+    }
+    
+    @IBAction func b32(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"影"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"影"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"影"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"影"
+        }
+        re = 1
+    }
+    
+    @IBAction func b33(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"指"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"指"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"指"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"指"
+        }
+        re = 1
+    }
+    
+    @IBAction func b34(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"狗"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"狗"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"狗"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"狗"
+        }
+        re = 1
+    }
+    
+    @IBAction func b35(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"韵"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"韵"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"韵"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"韵"
+        }
+        re = 1
+    }
+    
+    @IBAction func b41(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"犹"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"犹"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"犹"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"犹"
+        }
+        re = 1
+    }
+    
+    @IBAction func b42(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"嚎"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"嚎"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"嚎"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"嚎"
+        }
+    }
+    
+    @IBAction func b43(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"我"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"我"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"我"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"我"
+        }
+    }
+    
+    @IBAction func b44(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"鹿"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"鹿"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"鹿"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"鹿"
+        }
+    }
+    
+    @IBAction func b45(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"狼"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"狼"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"狼"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"狼"
+        }
+    }
+    
+    @IBAction func b51(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"语"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"语"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"语"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"语"
+        }
+    }
+    
+    @IBAction func b52(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"友"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"友"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"友"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"友"
+        }
+    }
+    
+    @IBAction func b53(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"不"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"不"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"不"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"不"
+        }
+    }
+    
+    @IBAction func b54(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"万"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"万"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"万"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"万"
+        }
+    }
+    
+    @IBAction func b55(_ sender: Any) {
+        if r1.text == ""
+        {
+            r1.text=r1.text!+"形"
+        }
+        else if r2.text == ""
+        {
+            r2.text=r2.text!+"形"
+        }
+        else if r3.text == ""
+        {
+            r3.text=r3.text!+"形"
+        }
+        else if r4.text == ""
+        {
+            r4.text=r4.text!+"形"
+        }
+    }
+    
+    
+    @IBAction func bq(_ sender: Any) {
+        if r1.text == "指"
+        {
+            if r2.text == "鹿"
+            {
+                if r3.text == "为"
+                {
+                    if r4.text == "马"
+                    {
+                        r6.text=""
+                        r6.text=r6.text!+"答案正确"
+                    }
+                    else
+                    {
+                        r6.text=""
+                        r6.text=r6.text!+"答案错误，请重新作答"
+                        r1.text=""
+                        r2.text=""
+                        r3.text=""
+                        r4.text=""
+                    }
+                    
+                }
+                else
+                {
+                    r6.text=""
+                    r6.text=r6.text!+"答案错误，请重新作答"
+                    r1.text=""
+                    r2.text=""
+                    r3.text=""
+                    r4.text=""
+                }
+            }
+            else
+            {
+                r6.text=""
+                r6.text=r6.text!+"答案错误，请重新作答"
+                r1.text=""
+                r2.text=""
+                r3.text=""
+                r4.text=""
+            }
+        }
+        else
+        {
+            r6.text=""
+            r6.text=r6.text!+"答案错误，请重新作答"
+            r1.text=""
+            r2.text=""
+            r3.text=""
+            r4.text=""
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let img = UIImage(named: "Pic1")
+        let imgView = UIImageView(image:img)
+        self.view.addSubview(imgView)
+        
     }
 
 
